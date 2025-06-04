24de3dc23c8181d83f31496d5b883d0b6e6e689d
F4bs04 <fabian.ajaraujo@gmail.com>
2025-06-04 19:19:20 -0300

    Adicionado componente Toaster do Sonner para exibir notificações de cópia

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "next-themes";
+import { Toaster } from "@/components/ui/sonner";
 import App from "./App.tsx";
 import "./index.css";
 
@@ -9,6 +10,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
       <App />
+      <Toaster position="top-right" richColors closeButton />
     </ThemeProvider>
   </StrictMode>,
 );
